71a59c2d5dfd18eebe4f3f014bd83e4ff449a6e7
Kleyton Lopes <kleyton.lop@gmail.com>
2023-04-01 11:25:55 -0300

    feat: migrating link to next/Link

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,15 @@
 import React from "react";
-
-const Link = ({ className, href, children }) => {
-  return (
-    <a href={href} className={className}>
-      {children}
-    </a>
-  );
-};
+import Link from 'next/link'
 
 function Card({ title, description, link }) {
   return (
     <div className="card">
       <h2>{title}</h2>
       <p>{description}</p>
-      <Link href={link}>Acessar</Link>
+      <Link href={`/quiz/${link}`}>Acessar</Link>
     </div>
   );
-}
+} 
 
 function Home() {
   return (
@@ -24,7 +17,7 @@ function Home() {
       <Card
         title="Exercício de conjunções"
         description="memorização de classificação de conjunções"
-        link="/conjunctions"
+        link="/conjuncoes"
       />
     </div>
   );
