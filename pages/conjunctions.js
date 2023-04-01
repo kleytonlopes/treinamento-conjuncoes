9618097f3cecd1a48b99f4f1a0804aac73ffa847
Kleyton Lopes <kleyton.lop@gmail.com>
2023-04-01 10:18:49 -0300

    feat: create index page and reallocating conjunctions page to another page

---
+++ pages/conjunctions.js
@@ -0,0 +1,91 @@
+import { useState, useEffect } from "react";
+import conjunctionsData from "./conjunctions.json";
+
+export default function Conjunctions() {
+  const [currentConjunctionIndex, setCurrentConjunctionIndex] = useState(-1);
+  const [hitCounter, setHitCounter] = useState(0);
+  const [missCounter, setMissCounter] = useState(0);
+  const [userAnswer, setUserAnswer] = useState("");
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const [showNewQuestion, setShowNewQuestion] = useState(true);
+
+  const conjunctions = conjunctionsData.conjunctions;
+  const currentConjunction = currentConjunctionIndex > -1 ? conjunctions[currentConjunctionIndex] : '';
+  
+  useEffect(() => {
+    setCurrentConjunctionIndex(Math.floor(Math.random() * conjunctionsData.conjunctions.length));
+  }, []);
+
+  function handleAnswer(event) {
+    const answer = event.target.value;
+    setUserAnswer(answer);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (
+      userAnswer.toLowerCase().trim() ===
+      currentConjunction.correctAnswer.classification.toLowerCase()
+    ) {
+      setHitCounter(hitCounter + 1)
+      setShowSuccess(true);
+      setTimeout(() => {
+        setShowSuccess(false);
+        setUserAnswer("");
+        setCurrentConjunctionIndex(
+          Math.floor(Math.random() * conjunctions.length)
+        );
+        setShowNewQuestion(true);
+      }, 1500);
+    } else {
+      setMissCounter(missCounter + 1)
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+        setUserAnswer("");
+      }, 3000);
+    }
+  }
+
+  return (
+    <div className="conjunctions-container">
+      <h1>Memorize a tabela de conjunções da língua portuguesa</h1>
+      <p>
+        Classifique a conjunção da seguinte frase em aditiva, adversativa,
+        alternativa, conclusiva, explicativa, causal, consecutiva, concessiva,
+        comparativa, condicional, conformativa, final, proporcional ou temporal:
+      </p>
+      {showNewQuestion && currentConjunction != '' &&(
+        <p
+          className="conjunctions-sentence"
+          dangerouslySetInnerHTML={{
+            __html: currentConjunction.sentence.replace(
+              currentConjunction.correctAnswer.conjunction,
+              `<strong>${currentConjunction.correctAnswer.conjunction}</strong>`
+            ),
+          }}
+        />
+      )}
+      <form onSubmit={handleSubmit} className="conjunctions-form">
+        <label>
+          Classificação:
+          <input
+            type="text"
+            value={userAnswer}
+            onChange={handleAnswer}
+            required
+            className="conjunctions-input"
+          />
+        </label>
+          <button type="submit" className="conjunctions-button">
+            Verificar
+          </button>
+       </form>
+      {showSuccess && <p className="conjunctions-success">Parabéns, você acertou!</p>}
+      {showError && <p className="conjunctions-error">Você errou, tente novamente.</p>}
+      {<p>Acertos: {hitCounter} - Erros: {missCounter}</p>}
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,92 +1,33 @@
-import { useState, useEffect } from "react";
-import conjunctionsData from "./conjunctions.json";
-//test
+import React from "react";
 
-export default function Conjunctions() {
-  const [currentConjunctionIndex, setCurrentConjunctionIndex] = useState(-1);
-  const [hitCounter, setHitCounter] = useState(0);
-  const [missCounter, setMissCounter] = useState(0);
-  const [userAnswer, setUserAnswer] = useState("");
-  const [showSuccess, setShowSuccess] = useState(false);
-  const [showError, setShowError] = useState(false);
-  const [showNewQuestion, setShowNewQuestion] = useState(true);
-
-  const conjunctions = conjunctionsData.conjunctions;
-  const currentConjunction = currentConjunctionIndex > -1 ? conjunctions[currentConjunctionIndex] : '';
-  
-  useEffect(() => {
-    setCurrentConjunctionIndex(Math.floor(Math.random() * conjunctionsData.conjunctions.length));
-  }, []);
-
-  function handleAnswer(event) {
-    const answer = event.target.value;
-    setUserAnswer(answer);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    if (
-      userAnswer.toLowerCase().trim() ===
-      currentConjunction.correctAnswer.classification.toLowerCase()
-    ) {
-      setHitCounter(hitCounter + 1)
-      setShowSuccess(true);
-      setTimeout(() => {
-        setShowSuccess(false);
-        setUserAnswer("");
-        setCurrentConjunctionIndex(
-          Math.floor(Math.random() * conjunctions.length)
-        );
-        setShowNewQuestion(true);
-      }, 1500);
-    } else {
-      setMissCounter(missCounter + 1)
-      setShowError(true);
-      setTimeout(() => {
-        setShowError(false);
-        setUserAnswer("");
-      }, 3000);
-    }
-  }
-
+const Link = ({ className, href, children }) => {
   return (
-    <div className="conjunctions-container">
-      <h1>Memorize a tabela de conjunções da língua portuguesa</h1>
-      <p>
-        Classifique a conjunção da seguinte frase em aditiva, adversativa,
-        alternativa, conclusiva, explicativa, causal, consecutiva, concessiva,
-        comparativa, condicional, conformativa, final, proporcional ou temporal:
-      </p>
-      {showNewQuestion && currentConjunction != '' &&(
-        <p
-          className="conjunctions-sentence"
-          dangerouslySetInnerHTML={{
-            __html: currentConjunction.sentence.replace(
-              currentConjunction.correctAnswer.conjunction,
-              `<strong>${currentConjunction.correctAnswer.conjunction}</strong>`
-            ),
-          }}
-        />
-      )}
-      <form onSubmit={handleSubmit} className="conjunctions-form">
-        <label>
-          Classificação:
-          <input
-            type="text"
-            value={userAnswer}
-            onChange={handleAnswer}
-            required
-            className="conjunctions-input"
-          />
-        </label>
-          <button type="submit" className="conjunctions-button">
-            Verificar
-          </button>
-       </form>
-      {showSuccess && <p className="conjunctions-success">Parabéns, você acertou!</p>}
-      {showError && <p className="conjunctions-error">Você errou, tente novamente.</p>}
-      {<p>Acertos: {hitCounter} - Erros: {missCounter}</p>}
+    <a href={href} className={className}>
+      {children}
+    </a>
+  );
+};
+
+function Card({ title, description, link }) {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <Link href={link}>Acessar</Link>
     </div>
   );
 }
+
+function Home() {
+  return (
+    <div className="home">
+      <Card
+        title="Exercício de conjunções"
+        description="memorização de classificação de conjunções"
+        link="/conjunctions"
+      />
+    </div>
+  );
+}
+
+export default Home;
